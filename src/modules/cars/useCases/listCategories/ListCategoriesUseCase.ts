f7bb3f09f8593a859d8ea4c834ae102e399a283a
felipeemoreno <contato@wr5solutions.com.br>
2022-04-17 18:04:57 -0300

    build: useCases_categories

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from '../../model/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+  constructor(private iCategoriesRepository: ICategoriesRepository) {}
+
+  execute(): Category[] {
+    const categories = this.iCategoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
